5aaec24a79bf01091302cb7010dd0b97b7430b33
Sarah Greeff <63438240+mrsSGhash@users.noreply.github.com>
2022-01-21 22:22:17 +0000

    Added a heading and subheading label. Checked that my label variables are named correctly.

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ window.resizable(width=False,height=False)
 window.title('Age Calculator!')
 
 # Labels for Heading and Subheadng of GUI
-
+lb_heading = tk.Label(window,text="The Age Calculator!",font=("Arial", 20),fg="black",bg="#F7DC6F")
+lb_subheading = tk.Label(window,font=("Arial",12),text="Enter your birthday which includes the day-month-year.",fg="black",bg="#F7DC6F")
  
 # Labels for date, month and year
 
