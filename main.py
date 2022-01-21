ac4e574a028fb1ac5d6c15a96efcb4b9172cbb97
SarahGreeff <63438240+mrsSGhash@users.noreply.github.com>
2022-01-21 22:20:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+''' AGE Calculator
+Source: https://www.askpython.com/python-modules/tkinter/age-calculator
+Date: 22/01/2022
+ '''
+# Creating a custom window
+
+
+# Labels for Heading and Subheadng of GUI
+
+ 
+# Labels for date, month and year
+
+
+# Entry boxes for date, month and year
+
+
+# Button to calculate age 
+
+ 
+# Label for text box that will display the calculated age
+
+
+# Button to exit application
+
+
+# Placing the elements on the screen
